049198d82a0e24acebf4fa6bc5b3675e426344de
gauravdevkar27 <devkardinesh18@gmail.com>
2025-08-26 23:36:30 +0530

    Added production build

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,28 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const auth = require('./routes/auth.routes'); 
 const todo = require('./routes/ToDo.routes');
+const path = require('path');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-
+//middleware
 app.use(cors());
 app.use(express.json());
+
+//api routes should come before static files
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Serve static files
 app.use('/api', auth);
 app.use('/api/todo', todo);
+
+// Database connection
 mongoose.connect(process.env.DB_URL).then((result) => {
     console.log("DB Connected Successfully");
 }).catch(err => {
     console.log(err);
 })
-
+// Start server
 app.listen(PORT, () => {
     console.log(`Server started at port : ${PORT}`);
 })
